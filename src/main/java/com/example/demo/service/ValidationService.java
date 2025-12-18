3cdbc42766ed810ab02f7ed26fac3cc4c4ca54b0
bala-amypo <bala@amypo.in>
2025-12-18 09:57:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ValidationService.java
@@ -0,0 +1,23 @@
+
+
+package com.example.demo.service.impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.ValidationService;
+import org.springframework.stereotype.Service;
+import com.example.demo.repository.ValidationRepository;
+import com.example.demo.entity.ValidationEntity;
+
+@Service
+public class ValidationServiceImpl implements ValidationService{
+
+@Autowired ValidationRepository value;
+
+@Override
+    public ValidationEntity postdata(ValidationEntity val){
+        
+        return value.save(val);
+
+    }
+
+}
+
