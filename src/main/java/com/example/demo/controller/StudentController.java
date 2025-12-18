b6ff52051c7961490d8885fe99f60196cbdf3be3
bala-amypo <bala@amypo.in>
2025-12-18 06:37:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
@@ -25,12 +26,15 @@ public class StudentController{
     public String deleteval(@PathVariable  int id){
         return ser.deleteData(id);
     }
-    @GetMapping("getId/{id}")
-    public StudentEntity getdata(@PthVariable int id){
+    @GetMapping("put/{id}")
+    public StudentEntity getdata(@PathVariable int id){
         return ser.getData(id);
     }           
     @PutMapping("/put/{id}")
-    public StudentEntity putvalue(@PathVariable int id,@RequestBody StudentEnt)
+    public StudentEntity putvalue(@PathVariable int id,@RequestBody StudentEntity entity){
+
+        return ser.updateDate(id,entity);
+    }
 
     
     
